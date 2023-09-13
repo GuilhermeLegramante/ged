835aa8b0d9c0a8d1d92e45489b75654c3c03634d
Guilherme <guilhermelegramante@gmail.com>
2023-09-13 13:38:07 -0300

    Migration de Documentos

---
+++ database/migrations/2023_09_13_132925_create_documents_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('user_id');
+            $table->integer('document_type_id')->nullable();
+            $table->string('number')->nullable();
+            $table->date('date')->nullable();
+            $table->text('note')->nullable();
+            $table->string('path')->unique();
+            $table->string('filename')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('document_type_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
