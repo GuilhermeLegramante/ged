47e3092985d18aa4621adb304be865d5c1ab6d88
Guilherme <guilhermelegramante@gmail.com>
2023-09-25 12:31:07 -0300

    Add colunas de início e fim de vigência do documento

---
+++ database/migrations/2023_09_25_122712_add_columns_to_documents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->date('validity_start')
+                ->nullable()
+                ->after('filename');
+            $table->date('validity_end')
+                ->nullable()
+                ->after('validity_start');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('documents', function (Blueprint $table) {
+            $table->dropColumn('validity_start');
+            $table->dropColumn('validity_end');
+        });
+    }
+}
